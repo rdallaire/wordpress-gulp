d14dc35b0946635c8dfa2ae4f19f94ac3953683d
Ross Dallaire <rdallaire@metergroup.com>
2019-07-18 17:10:39 -0700

    updates

--- page.php
+++ page.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
 <main id="content">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<div class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</div>
-</article>
-<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
-<?php endwhile; endif; ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
+    <div class="entry-content">
+        <?php the_content(); ?>
+    </div>
+    <ul class="menu">
+  <li><a href="#">One</a></li>
+  <li><a href="#">Two</a></li>
+  <li><a href="#">Three</a></li>
+  <li><a href="#">Four</a></li>
+</ul>
+    </article>
+    <?php endwhile; endif; ?>
 </main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
