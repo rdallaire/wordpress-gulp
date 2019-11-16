6135928d4c904d60f0d5ac27a1101c1630b9c651
Ross Dallaire <rdallaire@metergroup.com>
2019-11-16 07:56:41 -0800

    update SASS and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,135 @@
-var gulp = require('gulp');
-var del = require('del');
-var minifyCSS = require('gulp-csso');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+const gulp = require('gulp'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    browsersync = require('browser-sync').create(),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    inlinesource = require('gulp-inline-source'),
+    webp = require('gulp-webp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    replace = require('gulp-replace');
 
 sass.compiler = require('node-sass');
 
-var paths = {
+const paths = {
     styles: {
       src: 'src/scss/*.scss',
       dest: 'dist/css/'
-    }
-    // scripts: {
-    //   src: 'src/scripts/**/*.js',
-    //   dest: 'assets/scripts/'
-    // }
+    },
+    js : {
+        src: 'src/js/*.js',
+        dest: 'dist/js/'
+    },
+    img: {
+        src: 'src/img/**/*',
+        dest: 'site/img/'
+    },
+    php: {
+        src: './**/*.php'
+    },
   };
 
+// postcss plugins
+var pcssplugins = [
+    autoprefixer(),
+    cssnano()
+];
+
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+      proxy: 'localhost:8000'
+    });
+    done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
 function clean() {
-    // You can use multiple globbing patterns as you would with `gulp.src`,
-    // for example if you are using del 2.0 or above, return its promise
-    return del([ 'assets' ]);
+    return del([ 'site' ]);
   }
 
-function css() {
-    return gulp.src('src/scss/*.scss')
-      .pipe(sass())
-      .pipe(minifyCSS())
-      .pipe(reload({ stream:true }))
-      .pipe(gulp.dest(paths.styles.dest))
-      .pipe(reload({ stream:true }));
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(postcss(pcssplugins))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browsersync.stream());
 }
 
-function watch() {
-    browserSync({
-        proxy: 'localhost:8000'
-      });
-    gulp.watch(paths.styles.src, css);
+function stylesWatch() {
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browsersync.stream());
 }
 
-var build = gulp.series(css);
+function img() {
+    return gulp.src(paths.img.src)
+    // .pipe(image())
+    .pipe(gulp.dest(paths.img.dest));
+}
 
+function imgWebp() {
+    return gulp.src(paths.img.src)
+    // .pipe(image())
+    .pipe(webp({quality: 100}))
+    .pipe(gulp.dest(paths.img.dest));
+}
+
+// function html() {
+//     return gulp.src(paths.html.src)
+//     .pipe(fileinclude({
+//         prefix: '@@',
+//         basepath: '@file'
+//     }))
+//     .pipe(gulp.dest(paths.html.dest))
+//     .pipe(inlinesource())
+//     .pipe(htmlmin({
+//         collapseWhitespace: true,
+//         removeComments: true
+//     }))
+//     .pipe(gulp.dest(paths.html.dest));
+// }
+
+// function htmlWatch() {
+//     return gulp.src(paths.html.src)
+//     .pipe(replace('%%content%%', '@@include("./partials/pardot-form.html")'))
+//     .pipe(fileinclude({
+//         prefix: '@@',
+//         basepath: '@file'
+//       }))
+//     .pipe(gulp.dest(paths.html.dest));
+// }
+
+function js() {
+    return gulp.src(paths.js.src)
+    .pipe(gulp.dest(paths.js.dest))
+    .pipe(browsersync.stream());
+}
+
+// Watch files
+function watchFiles() {
+    gulp.watch(paths.styles.src, stylesWatch);
+    gulp.watch(paths.js.src, js);
+    gulp.watch(paths.php.src,
+        gulp.series(browserSyncReload)
+    );
+    gulp.watch(paths.img.src, img);
+    gulp.watch(paths.img.src, imgWebp);
+}
+
+var build = gulp.series(clean, styles, js, img, imgWebp);
+var preWatch = gulp.series(clean, stylesWatch, img, js, imgWebp);
+var watchActual = gulp.parallel(watchFiles, browserSync);
+var watch = gulp.series(preWatch, watchActual);
 
 exports.watch = watch;
 exports.default = build;
-
